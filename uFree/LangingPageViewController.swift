fd85acc121827b85a037613075b4625fbfc12e6b
Omer Winrauke <omerwinrauke@Yossefs-MBP.home>
2016-10-29 12:38:12 -0400

    First Commit and UI Skeleton For App

---
+++ uFree/LangingPageViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LangingPageViewController.swift
+//  uFree
+//
+//  Created by Omer Winrauke on 10/29/16.
+//  Copyright © 2016 Omer Winrauke. All rights reserved.
+//
+
+import UIKit
+
+class LangingPageViewController: UIViewController {
+    @IBOutlet weak var menuButton:UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
