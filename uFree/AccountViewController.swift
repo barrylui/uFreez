2279b762a5204155fc8d1376df8c22aaa484ac75
Omer Winrauke <omer921@gmail.com>
2016-11-01 22:18:04 -0400

    Added empty string catcher in the add friend alert box

--- uFree/AccountViewController.swift
+++ uFree/AccountViewController.swift
@@ -96,13 +96,14 @@ class AccountViewController: UIViewController, UITableViewDelegate, UITableViewD
         alert.addTextField { (textField) in
             textField.text = ""
         }
-        var textField = UITextField()
         // 3. Grab the value from the text field, and print it when the user clicks OK.
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_) in
-            textField = alert.textFields![0] // Force unwrapping because we know it exists.
-            CurrentUser.addToFriendsArray(friend: textField.text!)
-            ConnectionManager.addFriend(userName: CurrentUser.getUserName(), friendName: textField.text!)
-            self.friendsTable.reloadData()        }))
+            let textField = alert.textFields![0] // Force unwrapping because we know it exists.
+            if (textField.text != "") {
+                CurrentUser.addToFriendsArray(friend: textField.text!)
+                ConnectionManager.addFriend(userName: CurrentUser.getUserName(), friendName: textField.text!)
+                self.friendsTable.reloadData()}
+        }))
         self.present(alert, animated: true, completion: nil)
     }
     
