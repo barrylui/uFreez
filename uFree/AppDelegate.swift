2a584d1eeb74fed46d304e936012a9414c2e7e93
Omer Winrauke <omer921@gmail.com>
2016-11-18 11:18:43 -0500

    added badge icons

--- uFree/AppDelegate.swift
+++ uFree/AppDelegate.swift
@@ -39,6 +39,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        
+        UIApplication.shared.applicationIconBadgeNumber = 1
         print(userInfo)
     }
 
