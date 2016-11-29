03c01a6888d2a9d83c5519db0e148284dc65e090
Omer Winrauke <omer921@gmail.com>
2016-11-29 13:09:36 -0500

    app returns to landing page if it was sent to background

--- uFree/AppDelegate.swift
+++ uFree/AppDelegate.swift
@@ -59,6 +59,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyBoard.instantiateViewController(withIdentifier: "sw_reveal") as UIViewController
+        self.window?.rootViewController = viewController
+        self.window?.makeKeyAndVisible()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
